fae7c576e19a672429e811f33e34dcb48a7ee6ea
Nick Schoess <schoessnick@gmail.com>
2020-08-13 22:22:07 -0500

    took away table and search from the code and made them their own components

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,7 +1,5 @@
 import React from "react";
-// import Table from "./Table";
-import Table from "react-bootstrap/Table";
-import dataJSON from "../data.json";
+
 import "./style.css";
 
 export default function Container(props) {
@@ -14,44 +12,6 @@ export default function Container(props) {
                 <p className="lead">Search for an Employee, or filter by department!</p>
             </div>
         </div>
-
-        <div>
-            <input className="form-control search" type="text" placeholder="Search for an Employee"></input>
-            <button type="button" className="btn btn-primary">Search!</button>
-        </div>
-
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Role</th>
-                    <th>Github</th>
-                    <th>Dept</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    props.data.map(info => (
-                        <tr key={info.id}>
-                            <td>
-                                {info.name}
-                            </td>
-                            <td>
-                                {info.role}
-                            </td>
-                            <td>
-                                {info.Github}
-                            </td>
-                            <td>
-                                {info.Dept}
-                            </td>
-                        </tr>
-
-                    ))
-                }
-            </tbody>
-        </Table>
     </div>
     )
 }
